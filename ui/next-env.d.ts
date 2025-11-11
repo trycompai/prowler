b6e1c6ab62f8fd2d86bb0b75d6eec7dff1403620
Mariano Fuentes <marfuen98@gmail.com>
2025-11-11 18:53:09 -0500

    chore(ui): add Next.js type references for Node globals

---
+++ ui/next-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
+/// <reference types="react/experimental" />
+/// <reference types="react-dom/experimental" />
+/// <reference types="node" />
+
+// NOTE: This file should not be edited.
+// See https://nextjs.org/docs/app/building-your-application/configuring/typescript
+
